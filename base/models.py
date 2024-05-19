1ed5780434752d0e55ace6f63bffcc97833b4e88
ramiro350 <ramirofilho18@gmail.com>
2024-05-19 18:11:15 -0300

    feat: add price field to item model

--- base/models.py
+++ base/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
